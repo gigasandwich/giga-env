7644f9dd0354cb5651e5b049b64a93d41afe33b9
Yvan Noah <yvannandy@gmail.com>
2026-03-06 22:53:29 +0300

    Refactor: use OOP from now on

--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,25 @@
 #include <iostream>
 #include <windows.h>
+#include "EnvManager.h"
 
-std::string getEnvVar(HKEY scopeHKey, const std::string& varName);
-std::string setEnvVar(HKEY scopeHKey, const std::string& varName, const std::string& varValue);
-std::string deleteEnvVar(HKEY scopeHKey, const std::string& varName);
-void refreshEnvironment();
 
 /*
     Somehow HKEY_LOCAL_MACHINE doesn't work on both Create and Read
     Maybe an error about permissions ?
 */
 int main() {
+    EnvManager envManager;
+
     std::string varName = "JAVA_HOME";
-    std::string  varValue = setEnvVar(HKEY_CURRENT_USER, varName, "C:\\paths\\jdk\\jdk-21.0.9");
+    std::string  varValue = envManager.setEnvVar(varName, "C:\\paths\\jdk\\jdk-21.0.9");
     std::cout << varValue << std::endl;
 
-	deleteEnvVar(HKEY_CURRENT_USER, varName);
+    envManager.deleteEnvVar(varName);
 
-	varValue = getEnvVar(HKEY_CURRENT_USER, varName);
+	varValue = envManager.getEnvVar(varName);
 	std::cout << varValue << std::endl;
 
-    refreshEnvironment();
+    EnvManager::refreshEnvironment();
 
     return 0;
-}
-
-std::string getEnvVar(HKEY scopeHKey, const std::string& varName) {
-    HKEY hKey;
-
-    if (RegOpenKeyExA(scopeHKey, "Environment", 0, KEY_READ, &hKey) != ERROR_SUCCESS) {
-        return "";
-    }
-
-    char buffer[2048];
-    DWORD bufferSize = sizeof(buffer);
-    if (RegQueryValueExA(hKey, varName.c_str(), NULL, NULL, (LPBYTE)buffer, &bufferSize) != ERROR_SUCCESS) {
-        RegCloseKey(hKey);
-        return "";
-    }
-
-    RegCloseKey(hKey);
-
-    return std::string(buffer);
-}
-
-std::string setEnvVar(HKEY scopeHKey, const std::string& varName, const std::string& varValue) {
-    HKEY hKey;
-    
-    if (RegOpenKeyExA(scopeHKey, "Environment", 0, KEY_SET_VALUE, &hKey) != ERROR_SUCCESS) {
-        return ""; // TODO: throw exception ?
-    }
-
-    /**
-     * REG_EXPAND_SZ: string that may contain env vars
-     * varValue.size() + 1 because of "\0". Had to cast there too, otherwise we get a warning
-     */
-    RegSetValueExA(hKey, varName.c_str(), 0, REG_EXPAND_SZ, (BYTE*)varValue.c_str(), static_cast<DWORD>(varValue.size() + 1));
-    RegCloseKey(hKey);
-
-    return varValue;
-}
-
-std::string deleteEnvVar(HKEY scopeHKey, const std::string& varName) {
-    HKEY hKey;
-    
-    if (RegOpenKeyExA(scopeHKey, "Environment", 0, KEY_SET_VALUE, &hKey) != ERROR_SUCCESS) {
-        return ""; // TODO: throw exception ?
-    }
-
-    RegDeleteValueA(hKey, varName.c_str());
-    RegCloseKey(hKey);
-
-    return varName;
-}
-
-void refreshEnvironment()
-{
-    SendMessageTimeoutA(
-        HWND_BROADCAST,
-        WM_SETTINGCHANGE,
-        0,
-        (LPARAM)"Environment",
-        SMTO_ABORTIFHUNG,
-        5000,
-        NULL
-    );
 }
