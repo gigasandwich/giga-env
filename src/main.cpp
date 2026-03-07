68afc428074ee037088b2842ea57fd276a67295f
Yvan Noah <yvannandy@gmail.com>
2026-03-08 00:24:02 +0300

    Add OS check for the ideal implementation

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "env/EnvVarWindows.h"
 
 void mainTest();
+EnvVar* getEnvVarImpl(HKEY scopeHkey, std::string name);
 
 /*
     Somehow HKEY_LOCAL_MACHINE doesn't work on both Create and Read
@@ -15,7 +16,7 @@ int main() {
 }
 
 void mainTest() {
-    EnvVar* var = new EnvVarWindows(HKEY_CURRENT_USER, "JAVA_HOME");
+    EnvVar* var = getEnvVarImpl(HKEY_CURRENT_USER, "JAVA_HOME");
     std::cout << "Current value: " << var->value << std::endl;
 
     std::string newValue = var->setValue("C:\\paths\\jdk\\jdk-67");
@@ -23,4 +24,17 @@ void mainTest() {
 
     // std::string deletedVar = var->remove();
     // std::cout << "Deleted variable: " << deletedVar << " with value: " << var->value << std::endl;
+}
+
+EnvVar* getEnvVarImpl(HKEY scopeHkey, std::string name) {
+    EnvVar* instance;
+    #ifdef _WIN32
+        instance = new EnvVarWindows(scopeHkey, name);
+    #elif __linux__
+        //instance = new EnvVarLinux(scopeHkey, name);
+        throw std::runtime_error("Unsupported platform"); // For now :P
+    #else
+        throw std::runtime_error("Unsupported platform");
+    #endif
+    return instance;
 }
